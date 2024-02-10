d2b1fa050d1cf8610780435e973994cbd95593bd
Chiru S <85978862+Chiranth030@users.noreply.github.com>
2024-02-10 08:02:51 +0530

    Add files via upload

---
+++ sumoftwo.java
@@ -0,0 +1,8 @@
+public class sumoftwo{
+    public static void main(String[] args) {
+        int a=10;
+        int b =20;
+        int ans =a+b;
+        System.out.println("THE SUM OF TWO NUMBER " + a + b + " is " + ans);
+    }
+}
